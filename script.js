6a4798e269a42b47db1241ba5fd30dc6e783a091
lauryn <laurynmwase06@gmail.com>
2025-09-02 06:02:05 +0200

    made changes to the forms depending on database

---
+++ script.js
@@ -0,0 +1,19 @@
+
+//voter registration form
+const VoterFName=document.querySelector(".VFname");
+const VoterLName=document.querySelector(".VLname");
+const VoterNRC=document.querySelector("VNRC");
+const VoterGender=document.querySelector(".Vgender");
+
+
+
+
+const First_name=document.querySelector(".FName");
+First_name.textContent=VoterFName;
+const Last_name=document.querySelector(".LName");
+Last_name.textContent=VoterLName;
+const NRC=document.querySelector(".NRC");
+NRC.textContent=VoterNRC;
+const Gender=document.querySelector(".Gender");
+Gender.textContent=VoterGender;
+
