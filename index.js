107464c5680a208ace38ff41ce5ec632c747bb41
lauryn <laurynmwase06@gmail.com>
2025-10-02 12:42:40 +0200

    changed the endpoint index.html to login.html

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app =express();
 app.use(express.static(__dirname));
 
 app.get("/",(req, res)=>{
-    res.sendFile(path.join(__dirname, "/index.html"));
+    res.sendFile(path.join(__dirname, "/login.html"));
 });
 
 app.get("/candidate",(req,res)=>{
